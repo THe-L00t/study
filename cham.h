443f39845c32f0831364c9e22f82296b78149138
THe-L00t <sigun2001@naver.com>
2025-07-02 19:23:45 +0900

    cham 클래스 생성

---
+++ cham.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+class cham
+{
+public:
+	//¸â¹öÇÔ¼ö
+
+private:
+	//¸â¹öº¯¼ö
+	std::string name;
+	int hp, mp, speed, range;
+	std::string pos;
+};
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include "cham.h"
+
+int main() {
+	std::ifstream in{ "testdata.txt" };
+	if (not in) return 20250702;
+
+
+}
