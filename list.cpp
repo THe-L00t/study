fbe7772126074be4611a50b54ad2abd4bbb66702
THe-L00t <sigun2001@naver.com>
2025-07-03 15:15:22 +0900

    list 클래스 데이터 삽입 출력 선언 및 정의

--- list.cpp
+++ list.cpp
@@ -1,6 +1,20 @@
 #include "list.h"
 
+size_t list::size() {
+	return s;
+}
+
+size_t list::capacity()
+{
+	return cap;
+}
+
 void list::push_back(const cham& rhs)
 {
-	memcpy(&data[size++], &rhs, sizeof(cham));
+	memcpy(&data[s++], &rhs, sizeof(cham));
+}
+
+cham& list::operator[](int idx)
+{
+	return data[idx];
 }
--- list.h
+++ list.h
@@ -5,20 +5,24 @@ class list
 public:
 	list() = default;
 	list(size_t c) 
-		:capacity{c} {
-		data = new cham[capacity];
+		:cap{c} {
+		data = new cham[cap];
 	}
 	~list() {
 		if (data not_eq nullptr) delete[] data;
 	}
 
+	size_t size();
+	size_t capacity();
+
+
 	void push_back(const cham&);
 
-
+	cham& operator[](int idx);
 
 private:
 	cham* data{};
-	size_t size{};
-	size_t capacity{};
+	size_t s{};
+	size_t cap{};
 };
 
