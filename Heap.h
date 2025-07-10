ed312dc03ecb8e15ed6879676cc5d630331a567a
THe-L00t <sigun2001@naver.com>
2025-07-10 14:15:14 +0900

    Heap 클래스 생성

---
+++ Heap.h
@@ -0,0 +1,23 @@
+#pragma once
+class Heap
+{
+};
+
+/*
+public
+외부에서 접근 가능
+private
+외부에서 접근 불가능
+
+protected
+자식 클래스에서는 접근 가능하나, 외부에서는 접근 불가능 
+
+animal 큰 범위
+	^
+	|
+Dog 작은 범위
+
+
+일반 배열
+힙 배열, 정렬
+*/
